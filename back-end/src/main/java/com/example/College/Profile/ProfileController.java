44341930aa291df229a19d3aeca3c48d24560c52
AnnieStoyanova <ani100yanova777@gmail.com>
2023-06-10 23:10:12 +0300

    Add profile

--- back-end/src/main/java/com/example/College/Profile/ProfileController.java
+++ back-end/src/main/java/com/example/College/Profile/ProfileController.java
@@ -1,2 +1,49 @@
-package com.example.College.Profile;public class ProfileController {
+package com.example.College.Profile;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/profiles")
+public class ProfileController {
+
+    @Autowired
+    private ProfileService profileService;
+
+    @GetMapping("/{id}")
+    public Optional<Profile> getProfileById(@PathVariable Long id){
+        return profileService.findProfileById(id);
+    }
+
+    @GetMapping("/all")
+    private Collection<Profile> getAll(){
+        return profileService.findAllProfiles();
+    }
+
+    @GetMapping("/byEmail/{email}")
+    public Optional<Profile> getProfileByEmail(@PathVariable String email){
+        return profileService.findProfileByEmail(email);
+    }
+
+    @PostMapping("/add")
+    public void addProfile(@RequestBody Profile profile){
+        profileService.saveProfileToDb(profile);
+    }
+
+    @PutMapping("/update/{profileId}")
+    public Optional<Profile> updateProfile(@PathVariable("profileId") Long profileId, @RequestBody Profile newProfile) {
+        return profileService.findProfileById(profileId)
+                .map((profile) -> {
+                    profile.setName(newProfile.getName());
+                    profile.setEmail(newProfile.getEmail());
+                    profile.setPassword(newProfile.getPassword());
+                    profileService.saveProfileToDb(profile);
+                    return profile;
+                });
+
+    }
 }
+
--- back-end/src/main/java/com/example/College/Profile/ProfileRepo.java
+++ back-end/src/main/java/com/example/College/Profile/ProfileRepo.java
@@ -1,2 +1,12 @@
-package com.example.College.Profile;public class ProfileRepo {
+package com.example.College.Profile;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ProfileRepo extends JpaRepository<Profile,Long> {
+    Optional<Profile> findProfileById(Long id);
+    Optional<Profile> findProfileByEmail(String email);
 }
