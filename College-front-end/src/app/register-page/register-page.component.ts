a054824134b920a0bf13f7054345677b37f7031c
AnnieStoyanova <ani100yanova777@gmail.com>
2023-06-10 22:56:04 +0300

    Connect front end and back end

--- College-front-end/src/app/register-page/register-page.component.ts
+++ College-front-end/src/app/register-page/register-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ProfileService } from '../profile.service';
 
 @Component({
   selector: 'app-register-page',
@@ -6,5 +8,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./register-page.component.css']
 })
 export class RegisterPageComponent {
+  constructor(private profileService: ProfileService, private myRouter: Router){} 
+
+profile = {
+    id: 12,
+    name: " ",
+    email: " ",
+    password: " "
+    };
+
+getVal(name:string,email:string,password:string)
+  {
+
+      this.profile = {
+      id: 12,
+      name: name,
+      email: email,
+      password: password
+      };
+
+      this.profileService.addProfile(this.profile);
+      this.myRouter.navigateByUrl('home-page-component');
+
+  }
 
 }
+
